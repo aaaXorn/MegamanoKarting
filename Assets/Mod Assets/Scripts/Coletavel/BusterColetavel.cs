e5d9d6e0dcb64b455e009a6a6eeade4c3a752447
aaaXorn <70147479+aaaXorn@users.noreply.github.com>
2021-03-19 10:05:24 -0300

    pickups funcionais, battery e buster

---
+++ Assets/Mod Assets/Scripts/Coletavel/BusterColetavel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusterColetavel : ItemColetavel
+{
+    // Start is called before the first frame update
+    public override void Start()
+	{
+		audioS = GetComponent<AudioSource>();
+		pickup = "Buster";
+		
+		vetorRotacao = Vector3.up;
+		oscilacao = 0.001f;
+	}
+}
