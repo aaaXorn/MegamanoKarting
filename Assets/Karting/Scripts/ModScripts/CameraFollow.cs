fcfb9593b35181eabeb71a5fbb1c8876487af775
aaaXorn <lcrs.rp@gmail.com>
2021-03-24 19:09:09 -0300

    player 2 com camera

---
+++ Assets/Karting/Scripts/ModScripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public GameObject FollowTarget;
+	public GameObject LookAtTarget;
+	public float cameraVelocity = 3;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()//depois do update
+    {
+		//faz a camera seguir o player de uma forma mais fluida/suave
+        transform.position = Vector3.Lerp(transform.position, FollowTarget.transform.position, Time.smoothDeltaTime * cameraVelocity);
+		//faz a camera virar para a direcao do player
+		transform.LookAt(LookAtTarget.transform);
+    }
+}
